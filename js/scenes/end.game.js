b57a295008e08178862cb3ef23dca0b8f418abfa
Cristian Fernando Dávila <cristian.fernando.c.davila.lopez.-nd@disney.com>
2022-09-01 21:41:22 -0500

    End scene and complete game

--- js/scenes/end.game.js
+++ js/scenes/end.game.js
@@ -3,11 +3,39 @@ export class EndGame extends Phaser.Scene{
         super('endGameScene');
     }
 
-    preload(){
+    init(data){
+        this.playerQuantity = data.playerQuantity;
+        this.scorePlayer = data.scorePlayer;
+        this.scoreSecondPlayer = data.scoreSecondPlayer;
+        this.levelName = data.levelName;
+        this.modeName = data.modeName;
+    }
 
+    preload(){
+        this.load.baseURL = './';
+        this.load.image('jungle', '../img/background.png');
+        this.load.image('logo', '../img/JumpingMonkey.png');
+        this.load.image('monkey', '../img/monkey.png');
     }
     create(){
+        this.add.image(480, 320, 'jungle').setScale(2);
+        this.add.image(400,50, 'logo');
+        this.add.image(180,450, 'monkey');
 
+        this.add.text(100, 150, 'Player 1: ' + this.scorePlayer + ' Points', {fontSize: '32px', fill: '#fff'});
+        if(this.playerQuantity > 1) {
+            this.add.text(100, 230, 'Player 2: ' + this.scoreSecondPlayer + ' Points', {fontSize: '32px', fill: '#fff'});
+        }
+        this.add.text(400, 360, 'Level: ' + this.levelName, {fontSize: '32px', fill: '#fff'});
+        this.add.text(400, 400, 'Mode: ' + this.modeName, {fontSize: '32px', fill: '#fff'});
+
+        const backOption = this.add.zone(0, 0, 800, 530);
+        backOption.setOrigin(0);
+        backOption.setInteractive();
+        backOption.once('pointerdown', () => this.redirectScene('menuScene'));
+    }
+    redirectScene(sceneName) {
+        this.scene.start(sceneName);
     }
     update(){
 
--- js/scenes/main.scene.js
+++ js/scenes/main.scene.js
@@ -2,13 +2,22 @@ export class MainScene extends Phaser.Scene{
     constructor() {
         super('gameScene');
         this.level = 1;
-        this.playerQuantity = 2;
+        this.playerQuantity = 1;
         this.player = "";
         this.secondPlayer = "";
         this.scoreText = "";
         this.scoreTextPlayer2 = "";
         this.gameTime = 60;
         this.timer = 0;
+        this.goLeftP1 = false;
+        this.goLeftP2 = false;
+        this.goRightP1 = false;
+        this.goRightP2 = false;
+        this.goUpP1 = false;
+        this.goUpP2 = false;
+        this.musicStart = true;
+        this.levelName = 'Easy';
+        this.modeName = '1 Player';
     }
 
     /*init(data){
@@ -22,14 +31,24 @@ export class MainScene extends Phaser.Scene{
         this.load.image('ground', '../img/platform4.png');
         this.load.image('star', '../img/star.png');
         this.load.image('bomb', '../img/bomb.png');
-        this.load.image('controlPlayer1', '../img/Player1.png');
-        this.load.image('controlPlayer2', '../img/Player2.png');
+        this.load.image('controlsPlayer1', '../img/Player1.png');
+        this.load.image('controlsPlayer2', '../img/Player2.png');
 
         this.load.spritesheet('dude', '../img/dude.png', { frameWidth: 32, frameHeight: 48});
         this.load.spritesheet('secondPlayer', '../img/secondPlayer.png', { frameWidth: 32, frameHeight: 48});
+
+        this.load.audio('music', '../sounds/Banana_Craziness.mp3')
+        this.load.audio('getStar', '../sounds/Rise06.mp3')
+        this.load.audio('crash', '../sounds/bzzzt.wav')
     }
     create(){
         let self = this;
+
+        if(this.musicStart){
+            this.musicStart = false;
+            const music = this.sound.add('music');
+            //music.play({volume:0.15, loop:true});
+        }
         this.add.image(400,265, 'jungle').setScale(2);
         let platforms = this.physics.add.staticGroup();
         platforms.create(180, 530, 'ground');
@@ -61,6 +80,7 @@ export class MainScene extends Phaser.Scene{
         this.physics.add.collider(this.player, platforms);
 
         if(this.playerQuantity > 1) {
+            this.modeName = "2 Players";
             this.secondPlayer = this.physics.add.sprite(500, 450, 'secondPlayer');
             this.secondPlayer.setCollideWorldBounds(true);
             this.secondPlayer.setBounce(0.2);
@@ -92,15 +112,18 @@ export class MainScene extends Phaser.Scene{
         }
         function collectStar(player, star){
             this.player.score += 10;
-            colliderStar(star);
+            colliderStar(star, this);
             this.scoreText.setText('P1 score: ' + this.player.score);
         }
         function collectStarPlayer2(player, star){
             this.secondPlayer.score += 10;
-            colliderStar(star);
+            colliderStar(star, this);
             this.scoreTextPlayer2.setText('P2 score: ' + this.secondPlayer.score);
         }
-        function colliderStar(star){
+        function colliderStar(star, context){
+            const musicStar = context.sound.add('getStar');
+            musicStar.play({volume:1, loop: false});
+
             star.disableBody(true, true);
             if(stars.countActive(true) === 0){
                 let bomb = bombs.create(Phaser.Math.Between(0, 800), 16, 'bomb');
@@ -114,6 +137,9 @@ export class MainScene extends Phaser.Scene{
             }
         }
         function hitBomb(element, bomb){
+            const musicCrash = self.sound.add('crash');
+            musicCrash.play({volume:1, loop: false});
+
             if(this.playerQuantity === 1) {
                 this.physics.pause();
                 this.player.setTint(0xff0000);
@@ -126,17 +152,32 @@ export class MainScene extends Phaser.Scene{
             }
         }
         function hitBombPlayer2(element, bomb){
+            const musicCrash = self.sound.add('crash');
+            musicCrash.play({volume:1, loop: false});
             this.secondPlayer.score = this.secondPlayer.score -50 <= 0 ? 0 : this.secondPlayer.score - 50;
             this.scoreTextPlayer2.setText('P2 score: ' + this.secondPlayer.score);
         }
         function endScene(){
-            self.time.addEvent({
+            self.scene.start('endGameScene', {
+                playerQuantity: self.playerQuantity,
+                scorePlayer: self.player.score,
+                scoreSecondPlayer: self.secondPlayer.score,
+                levelName: self.levelName,
+                modeName: self.modeName
+            });
+            /*self.time.addEvent({
                 delay: 1500,
                 loop: false,
                 callBack: () => {
-                    self.scene.start("endScene");
+                    self.scene.start('endGameScene', {
+                        playerQuantity: self.playerQuantity,
+                        scorePlayer: self.player.score,
+                        scoreSecondPlayer: self.secondPlayer.score,
+                        levelName: self.levelName,
+                        modeName: self.modeName
+                    });
                 }
-            });
+            });*/
         }
         this.anims.create({
             key: 'left',
@@ -176,6 +217,77 @@ export class MainScene extends Phaser.Scene{
             repeat: -1
         });
         this.scoreText = this.add.text(16, 16, 'P1 score: 0', { fontSize: '32px', fill: '#000'});
+
+        /* MOBILE CONTROLS */
+        if(screen.width <= 900){
+            this.add.image(100, 450, 'controlsPlayer1').setScale(0.5);
+            const leftOptionP1 = self.add.zone(15, 420, 50, 50);
+            leftOptionP1.setOrigin(0);
+            leftOptionP1.setInteractive();
+            leftOptionP1.on('pointerdown', () => setLeftP1(true));
+            leftOptionP1.on('pointerup', () => setLeftP1(false));
+            leftOptionP1.on('pointerout', () => setLeftP1(false));
+            //this.add.graphics().lineStyle(2, 0xff0000).strokeRectShape(leftOptionP1);
+            const rightOptionP1 = self.add.zone(130, 420, 50, 50);
+            rightOptionP1.setOrigin(0);
+            rightOptionP1.setInteractive();
+            rightOptionP1.on('pointerdown', () => setRightP1(true));
+            rightOptionP1.on('pointerup', () => setRightP1(false));
+            rightOptionP1.on('pointerout', () => setRightP1(false));
+            //this.add.graphics().lineStyle(2, 0xff0000).strokeRectShape(rightOptionP1);
+            const upOptionP1 = self.add.zone(75, 395, 50, 50);
+            upOptionP1.setOrigin(0);
+            upOptionP1.setInteractive();
+            upOptionP1.on('pointerdown', () => setUpP1(true));
+            upOptionP1.on('pointerup', () => setUpP1(false));
+            upOptionP1.on('pointerout', () => setUpP1(false));
+            //this.add.graphics().lineStyle(2, 0xff0000).strokeRectShape(upOptionP1);
+
+            if(self.playerQuantity > 1){
+                this.add.image(700, 450, 'controlsPlayer2').setScale(0.5);
+                const leftOptionP2 = self.add.zone(615, 420, 50, 50);
+                leftOptionP2.setOrigin(0);
+                leftOptionP2.setInteractive();
+                leftOptionP2.on('pointerdown', () => setLeftP2(true));
+                leftOptionP2.on('pointerup', () => setLeftP2(false));
+                leftOptionP2.on('pointerout', () => setLeftP2(false));
+                //this.add.graphics().lineStyle(2, 0xff0000).strokeRectShape(leftOptionP2);
+                const rightOptionP2 = self.add.zone(730, 420, 50, 50);
+                rightOptionP2.setOrigin(0);
+                rightOptionP2.setInteractive();
+                rightOptionP2.on('pointerdown', () => setRightP2(true));
+                rightOptionP2.on('pointerup', () => setRightP2(false));
+                rightOptionP2.on('pointerout', () => setRightP2(false));
+                //this.add.graphics().lineStyle(2, 0xff0000).strokeRectShape(rightOptionP2);
+                const upOptionP2 = self.add.zone(675, 395, 50, 50);
+                upOptionP2.setOrigin(0);
+                upOptionP2.setInteractive();
+                upOptionP2.on('pointerdown', () => setUpP2(true));
+                upOptionP2.on('pointerup', () => setUpP2(false));
+                upOptionP2.on('pointerout', () => setUpP2(false));
+                //this.add.graphics().lineStyle(2, 0xff0000).strokeRectShape(upOptionP2);
+            }
+
+            function setLeftP1(status){
+                self.goLeftP1 = status;
+            }
+            function setLeftP2(status){
+                self.goLeftP2 = status;
+            }
+            function setRightP1(status){
+                self.goRightP1 = status;
+            }
+            function setRightP2(status){
+                self.goRightP2 = status;
+            }
+            function setUpP1(status){
+                self.goUpP1= status;
+            }
+            function setUpP2(status){
+                self.goUpP2 = status;
+            }
+        }
+        /* MOBILE CONTROLS */
     }
 
     refreshTime(){
@@ -204,34 +316,44 @@ export class MainScene extends Phaser.Scene{
         }
     }
 
-    update(time, delta){
-        this.timer += delta;
-        while (this.timer > 1000) {
-            this.timer -= 1000;
-            this.gameTime --;
+    update(time, delta) {
+        const self = this;
 
-            this.timeText.setText(this.gameTime);
-            if(this.gameTime === 0){
-                this.physics.pause();
-                this.player.setTint(0xff0000);
-                this.secondPlayer.setTint(0xff0000);
+        if(this.playerQuantity > 1){
+            this.timer += delta;
+            while (this.timer > 1000) {
+                this.timer -= 1000;
+                this.gameTime--;
 
-                this.time.addEvent({
-                    delay: 1500,
-                    loop: false,
-                    callback: () => {
-                        this.scene.start("endScene");
-                    }
-                });
+                this.timeText.setText(this.gameTime);
+                if (this.gameTime === 0) {
+                    this.physics.pause();
+                    this.player.setTint(0xff0000);
+                    this.secondPlayer.setTint(0xff0000);
+
+                    this.time.addEvent({
+                        delay: 1500,
+                        loop: false,
+                        callback: () => {
+                            self.scene.start('endGameScene', {
+                                playerQuantity: self.playerQuantity,
+                                scorePlayer: self.player.score,
+                                scoreSecondPlayer: self.secondPlayer.score,
+                                levelName: self.levelName,
+                                modeName: self.modeName
+                            });
+                        }
+                    });
+                }
             }
         }
         let cursors = this.input.keyboard.createCursorKeys();
 
-        if(cursors.left.isDown){
+        if(cursors.left.isDown || this.goLeftP1){
             this.player.setVelocityX(-160);
             this.player.anims.play('left', true);
         }
-        else if(cursors.right.isDown){
+        else if(cursors.right.isDown || this.goRightP1){
             this.player.setVelocityX(160);
             this.player.anims.play('right', true);
         }
@@ -239,7 +361,7 @@ export class MainScene extends Phaser.Scene{
             this.player.setVelocityX(0);
             this.player.anims.play('turn', true);
         }
-        if(cursors.up.isDown && this.player.body.touching.down){
+        if((cursors.up.isDown || this.goUpP1) && this.player.body.touching.down){
             this.player.setVelocityY(-330);
         }
 
@@ -251,11 +373,11 @@ export class MainScene extends Phaser.Scene{
             let keyObjLeft = this.input.keyboard.addKey('A');
             let player2Left = keyObjLeft.isDown;
 
-            if(player2Left){
+            if(player2Left || this.goLeftP2){
                 this.secondPlayer.setVelocityX(-160);
                 this.secondPlayer.anims.play('leftP2', true);
             }
-            else if(player2Right){
+            else if(player2Right || this.goRightP2){
                 this.secondPlayer.setVelocityX(160);
                 this.secondPlayer.anims.play('rightP2', true);
             }
@@ -263,7 +385,7 @@ export class MainScene extends Phaser.Scene{
                 this.secondPlayer.setVelocityX(0);
                 this.secondPlayer.anims.play('turnP2', true);
             }
-            if(player2Up && this.secondPlayer.body.touching.down){
+            if((player2Up || this.goUpP2) && this.secondPlayer.body.touching.down){
                 this.secondPlayer.setVelocityY(-330);
             }
         }
